706de922752a2b62e8be712ebe72f6b9faa170bb
Sophia Zhang <szzhang@andrew.cmu.edu>
2023-04-05 11:15:45 -0400

    how to get WiFi MAC address for NodeMCU board

---
+++ get_MAC/get_MAC.ino
@@ -0,0 +1,11 @@
+#include <ESP8266WiFi.h>
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+}
+ 
+void loop(){
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+}
