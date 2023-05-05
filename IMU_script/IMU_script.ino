2e6c4a6bd0aba2eaa37971290b0d59c7baa7255f
Sophia Zhang <szzhang@andrew.cmu.edu>
2023-05-05 16:48:03 -0400

    changed thing to rotational data

--- IMU_script/IMU_script.ino
+++ IMU_script/IMU_script.ino
@@ -17,25 +17,32 @@ int prevBState = 0; // temporary variable for reading the button pin status (the
 int cState = 0;      // temporary variable
 int cal_time = 1;    // length of calibration
 
-float quant_vector[4] = {0, 0, 0}; //w, x, y, z coordinates
-float pos_vec[3] = {0, 0, 0}; 
-float vel_vec[3] = {0, 0, 0}; 
-float acc_error[3] = {0, 0, 0}; 
-float acc_mag = 0; 
-int calibration = 0; 
+float x_distance = 0; 
+float y_distance = 0; 
 
-// Values used to track time
+//time tracking
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
 int interval = 0; //ms
 
-//collect the acceleration error
-void cal_pen(int dt) {
-  imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  acc_error[0] += acc.x();
-  acc_error[1] += acc.y(); 
-  acc_error[2] += acc.z(); 
-}
+// orientation data
+float thetaM;
+float phiM;
+float thetaFold=0;
+float thetaFnew;
+float phiFold=0;
+float phiFnew;
+ 
+float thetaG=0;
+float phiG=0;
+ 
+float theta;
+float phi;
+ 
+float dt;
+unsigned long millisOld;
+ 
+#define BNO055_SAMPLERATE_DELAY_MS (100)
 
 void setup() {
   Serial.begin(9600);
@@ -55,28 +62,6 @@ void setup() {
   bno.setExtCrystalUse(true);
 } 
 
-//** QUATERNION FUNCTIONS **//
-void quat_prod(float q1[4], float q2[4], float q3[4]) {
-  q3[0] = q1[0]*q2[0] - q1[1]*q2[1] - q1[2]*q2[2] - q1[3]*q2[3];
-  q3[1] = q1[0]*q2[1] + q1[1]*q2[0] + q1[2]*q2[3] - q1[3]*q2[2];
-  q3[2] = q1[0]*q2[2] - q1[1]*q2[3] + q1[2]*q2[0] + q1[3]*q2[1];
-  q3[3] = q1[0]*q2[3] + q1[1]*q2[2] - q1[2]*q2[1] + q1[3]*q2[0];
-}
-
-void quat_conj(float q1[4]) {
-  q1[1] = q1[1] * -1; 
-  q1[2] = q1[2] * -1;
-  q1[3] = q1[3] * -1;; 
-}
-
-void quat_rotate(float v[4], float q[4], float res[4]) {
-  float temp[4] = {0, 0, 0, 0}; 
-  float conj_q[4] = {q[0], q[1], q[2], q[3]};
-  quat_conj(conj_q);
-  quat_prod(q, v, temp);
-  quat_prod(temp, conj_q, res);
-}
-//** QUATERNION FUNCTIONS **//
 //** TIME FUNCTIONS **//
 bool hit_time_interval(){
   bool hasHit = currentMillis - previousMillis >= interval;
@@ -92,12 +77,29 @@ void update_time(){
 //** TIME FUNCTIONS **//
 
 void loop() {
-  long curr = millis(); 
   sensors_event_t event; 
   prevBState = bState;
   bState = digitalRead(button);
   cState = digitalRead(cal_button); 
 
+  // collect and calculate the roll and pitch data
+  uint8_t system, gyro, accel, mg = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mg);
+  imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu::Vector<3> gyr = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+ 
+  thetaM=-atan2(acc.x()/9.8,acc.z()/9.8)/2/3.141592654*360;
+  phiM=-atan2(acc.y()/9.8,acc.z()/9.8)/2/3.141592654*360;
+  phiFnew=.95*phiFold+.05*phiM;
+  thetaFnew=.95*thetaFold+.05*thetaM;
+ 
+  dt=(millis()-millisOld)/1000.;
+  millisOld=millis();
+  theta=(theta+gyr.y()*dt)*.95+thetaM*.05;
+  phi=(phi-gyr.x()*dt)*.95+ phiM*.05;
+  thetaG=thetaG+gyr.y()*dt;
+  phiG=phiG-gyr.x()*dt;
+  
   //bluetooth signals
   if (cState == LOW && prevBState == LOW && bState == HIGH){
       Serial.print("Bon\n");
@@ -108,63 +110,36 @@ void loop() {
   //trying to calibrate the pen
   if (cState == HIGH) {
     digitalWrite(c_led, HIGH);
-    cal_pen(cal_time); 
-    cal_time++; 
+    thetaG=0;
+    thetaFold=0;
+    phiG=0;
+    phiFold=0;
+    x_distance = 0; 
+    y_distance = 0; 
   } else { 
     digitalWrite(c_led, LOW);
-    acc_error[0] /= cal_time;
-    acc_error[1] /= cal_time;
-    acc_error[2] /= cal_time;
-    cal_time = 1; 
   }
 
   // when drawing button is pressed and not calibrating
   if (bState == HIGH && cState == LOW) {
     digitalWrite(led, HIGH);
-    long last_milli = millis();
-    imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-    imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-    imu::Quaternion quat = bno.getQuat(); 
-
-    float acc_vect[4] = {0, acc.x() - acc_error[0], acc.y() - acc_error[1], acc.z() - acc_error[2]}; 
-    float quat_vect[4] = {quat.w(), quat.x(), quat.y(), quat.z()};
-    float res[4] = {0, 0, 0, 0}; 
-
-    float conj_q[4] = {quat_vect[0], quat_vect[1], quat_vect[2], quat_vect[3]};
-    quat_conj(conj_q);
-    quat_rotate(acc_vect, conj_q, res); 
-
-    //convert into m/s/s
-    res[1] /= 1000000L;
-    res[2] /= 1000000L;
-    res[3] /= 1000000L;
-    long curr_milli = millis();
-    long dt = (curr_milli - last_milli); 
-    //get position by float integrating
-    vel_vec[0] += res[1] * dt;
-    vel_vec[1] += res[2] * dt;
-    vel_vec[2] += res[3] * dt;
-
-    pos_vec[0] += vel_vec[0] * dt;
-    pos_vec[1] += vel_vec[1] * dt;
-    pos_vec[2] += vel_vec[2] * dt;
-
     update_time();
     if (hit_time_interval()) {
-      Serial.print(pos_vec[0], 2);
-      Serial.print("_");
-      Serial.print(pos_vec[2], 2);
-      Serial.print("_");
-      Serial.print(pos_vec[1], 2);
-      Serial.print("\n");
+      x_distance += -phi * dt * dt / 100;
+      y_distance += theta * dt * dt / 100;
+      Serial.print(x_distance, 2); 
+      Serial.print("_"); 
+      Serial.print(y_distance, 2);
+      Serial.print("_"); 
+      Serial.print(0);
+      Serial.print("\n"); 
     }
+    phiFold=phiFnew;
+    thetaFold=thetaFnew;
   } else { 
     digitalWrite(led, LOW);
-    pos_vec[0] = 0;
-    pos_vec[1] = 0;
-    pos_vec[2] = 0;
+    x_distance = 0; 
+    y_distance = 0; 
   }
-  long curr1 = millis(); 
-  //Serial.println(curr1 - curr); 
-  delay(100); //how long IMU delay is
+  delay(BNO055_SAMPLERATE_DELAY_MS);
 }
